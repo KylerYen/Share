13c9801f02b98b23d5f0207ca24f81fa143c1abf
larry <qasw77564@gmail.com>
2022-10-12 20:38:11 +0800

    測試

--- OrderMenu/MainWindow.xaml.cs
+++ OrderMenu/MainWindow.xaml.cs
@@ -24,5 +24,38 @@ namespace OrderMenu
         {
             InitializeComponent();
         }
+
+        private void Item1_Chk_Checked(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine(sender);
+        }
+
+        private void SendOrder_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            List<Menu> list = new List<Menu>() { };
+            bool check1, check2, check3;
+            check1 = (bool)Item1_Chk.IsChecked;
+            check2 = (bool)Item2_Chk.IsChecked;
+            check3 = (bool)Item3_Chk.IsChecked;
+            if (check1 != true && check2 != true && check3 != true)
+            {
+                list.Add(new Menu { animals = "", content =""});
+                lb.Content = "請勾選菜單";
+            }
+            else {
+                lb.Content = "已選擇";
+            }
+        }
+    }
+
+    public class Menu { 
+        public string animals { get; set; }
+        public string content { get; set; }
+    }
+
+    public class Menu2
+    {
+        public string title { get; set; }
+        public string content { get; set; }
     }
 }
