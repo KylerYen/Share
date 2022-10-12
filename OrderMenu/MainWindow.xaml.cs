e644cd7be40c2e12fd3b7cb090fbeb5684a5ac45
KylerYen <kyleryen88@gmail.com>
2022-10-12 21:55:46 +0800

    使用List初版，感覺作法用錯了 2022/10/12

--- OrderMenu/MainWindow.xaml.cs
+++ OrderMenu/MainWindow.xaml.cs
@@ -34,21 +34,51 @@ namespace OrderMenu
         {
             List<Menu> list = new List<Menu>() { };
             bool check1, check2, check3;
+            int Item1, Item2, Item3;
+            int Price1, Price2, Price3;
+
+            Item1 = int.Parse(Item1_txtbox.Text.ToString());
+            Item2 = int.Parse(Item2_txtbox.Text.ToString());
+            Item3 = int.Parse(Item3_txtbox.Text.ToString());
+
             check1 = (bool)Item1_Chk.IsChecked;
             check2 = (bool)Item2_Chk.IsChecked;
             check3 = (bool)Item3_Chk.IsChecked;
+
+            Price1 = Item1 * 30;
+            Price2 = Item2 * 35;
+            Price3 = Item3 * 40;
+
             if (check1 != true && check2 != true && check3 != true)
             {
-                list.Add(new Menu { animals = "", content =""});
+                //list.Add(new Menu { animals = "", content =""});
                 lb.Content = "請勾選菜單";
             }
-            else {
-                lb.Content = "已選擇";
+            else if(check1 == true && check2 == true && check3 == true)
+            {
+                list.Add(new Menu { Item = "魯肉飯", Quantity = Item1, Price = Price1 });
+                list.Add(new Menu { Item = "肉圓", Quantity = Item2, Price = Price2 });
+                list.Add(new Menu { Item = "麵線", Quantity = Item3, Price = Price3 });
+
+                MessageBox.Show("總共點了:"+"\n"+list[0].Item+ list[0].Quantity+"碗"+ list[0].Price+"元"+"\n" + list[1].Item + list[1].Quantity + "個" + list[1].Price + "元" + "\n" + list[2].Item + list[2].Quantity + "碗" + list[2].Price + "元" + "\n");
+            }
+            else{
+                //lb.Content = "已選擇";
+                if (check1 == true) 
+                {
+
+                }
             }
         }
     }
 
-    public class Menu { 
+    public class Menu
+    {
+        public String Item;//項目
+        public int Quantity;//總數量
+        public int Price;//總價
+    }
+    /*public class Menu { 
         public string animals { get; set; }
         public string content { get; set; }
     }
@@ -57,5 +87,5 @@ namespace OrderMenu
     {
         public string title { get; set; }
         public string content { get; set; }
-    }
+    }*/
 }
